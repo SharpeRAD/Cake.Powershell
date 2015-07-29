21149a691c7ab9c6c6883fc2bf57c025338f4712
Phillip Sharpe <code@sharperad.com>
2015-07-29 23:10:10 +0100

    Move imports

--- src/Powershell/Aliases/CakeAliases.cs
+++ src/Powershell/Aliases/CakeAliases.cs
@@ -14,6 +14,7 @@
 namespace Cake.Powershell
 {
     [CakeAliasCategory("Powershell")]
+    [CakeNamespaceImport("System.Management.Automation")]
     public static class CakeAliases
     {
         /// <summary>
@@ -23,7 +24,6 @@ namespace Cake.Powershell
         /// <param name="script">The powershell script to run.</param>
         /// <returns>A collection of powershell objects</returns>
         [CakeMethodAlias]
-        [CakeNamespaceImport("System.Management.Automation")]
         public static Collection<PSObject> StartPowershellScript(this ICakeContext context, string script)
         {
             return new PowershellRunner(context.Environment, context.Log).Start(script, new PowershellSettings());
@@ -37,7 +37,6 @@ namespace Cake.Powershell
         /// <param name="arguments">The arguments to append.</param>
         /// <returns>A collection of powershell objects</returns>
         [CakeMethodAlias]
-        [CakeNamespaceImport("System.Management.Automation")]
         public static Collection<PSObject> StartPowershellScript(this ICakeContext context, string script, Action<ProcessArgumentBuilder> arguments)
         {
             return new PowershellRunner(context.Environment, context.Log).Start(script, new PowershellSettings().WithArguments(arguments));
@@ -51,7 +50,6 @@ namespace Cake.Powershell
         /// <param name="settings">The information about the script to start.</param>
         /// <returns>A collection of powershell objects</returns>
         [CakeMethodAlias]
-        [CakeNamespaceImport("System.Management.Automation")]
         public static Collection<PSObject> StartPowershellScript(this ICakeContext context, string script, PowershellSettings settings)
         {
             return new PowershellRunner(context.Environment, context.Log).Start(script, settings);
@@ -66,7 +64,6 @@ namespace Cake.Powershell
         /// <param name="path">The path of the script file to run.</param>
         /// <returns>A collection of powershell objects</returns>
         [CakeMethodAlias]
-        [CakeNamespaceImport("System.Management.Automation")]
         public static Collection<PSObject> StartPowershellFile(this ICakeContext context, FilePath path)
         {
             return new PowershellRunner(context.Environment, context.Log).Start(path, new PowershellSettings());
@@ -80,7 +77,6 @@ namespace Cake.Powershell
         /// <param name="arguments">The arguments to append.</param>
         /// <returns>A collection of powershell objects</returns>
         [CakeMethodAlias]
-        [CakeNamespaceImport("System.Management.Automation")]
         public static Collection<PSObject> StartPowershellFile(this ICakeContext context, FilePath path, Action<ProcessArgumentBuilder> arguments)
         {
             return new PowershellRunner(context.Environment, context.Log).Start(path, new PowershellSettings().WithArguments(arguments));
@@ -94,7 +90,6 @@ namespace Cake.Powershell
         /// <param name="settings">The information about the script to start.</param>
         /// <returns>A collection of powershell objects</returns>
         [CakeMethodAlias]
-        [CakeNamespaceImport("System.Management.Automation")]
         public static Collection<PSObject> StartPowershellFile(this ICakeContext context, FilePath path, PowershellSettings settings)
         {
             return new PowershellRunner(context.Environment, context.Log).Start(path, settings);
@@ -110,7 +105,6 @@ namespace Cake.Powershell
         /// <param name="path">The temporary path to download the file to.</param>
         /// <returns>A collection of powershell objects</returns>
         [CakeMethodAlias]
-        [CakeNamespaceImport("System.Management.Automation")]
         public static Collection<PSObject> StartPowershellDownload(this ICakeContext context, Uri uri, FilePath path)
         {
             return new PowershellRunner(context.Environment, context.Log).Start(uri, path, new PowershellSettings());
@@ -125,7 +119,6 @@ namespace Cake.Powershell
         /// <param name="arguments">The arguments to append.</param>
         /// <returns>A collection of powershell objects</returns>
         [CakeMethodAlias]
-        [CakeNamespaceImport("System.Management.Automation")]
         public static Collection<PSObject> StartPowershellDownload(this ICakeContext context, Uri uri, FilePath path, Action<ProcessArgumentBuilder> arguments)
         {
             return new PowershellRunner(context.Environment, context.Log).Start(uri, path, new PowershellSettings().WithArguments(arguments));
@@ -140,7 +133,6 @@ namespace Cake.Powershell
         /// <param name="settings">The information about the script to start.</param>
         /// <returns>A collection of powershell objects</returns>
         [CakeMethodAlias]
-        [CakeNamespaceImport("System.Management.Automation")]
         public static Collection<PSObject> StartPowershellDownload(this ICakeContext context, Uri uri, FilePath path, PowershellSettings settings)
         {
             return new PowershellRunner(context.Environment, context.Log).Start(uri, path, settings);
