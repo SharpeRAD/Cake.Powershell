a949d013c78835fbbd03f9f1b84cfddb9d28055a
SharpeRAD <code@sharperad.com>
2016-07-05 21:31:43 +0100

    Debug logging

--- src/PowerShell.Tests/Utils/DebugLog.cs
+++ src/PowerShell.Tests/Utils/DebugLog.cs
@@ -49,8 +49,17 @@ namespace Cake.Powershell.Tests
                     DebugLog.Lines = new List<string>();
                 }
 
+                format = String.Format(format, args);
                 DebugLog.Lines.Add(format);
-                Debug.WriteLine(format);
+
+                if (Debugger.IsAttached)
+                {
+                    Debug.WriteLine(format);
+                }
+                else
+                {
+                    Console.WriteLine(format);
+                }
             }
             catch { }
         }
